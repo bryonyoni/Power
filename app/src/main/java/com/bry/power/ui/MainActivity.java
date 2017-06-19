1bfe8eb222f28364a491c5db4b4a0a7ad92095d6
bryonyoni <bryonyoni@gmail.com>
2017-06-19 12:57:53 +0300

    Added a playAudio method
    Added a method that will initiate and play media files.

--- app/src/main/java/com/bry/power/ui/MainActivity.java
+++ app/src/main/java/com/bry/power/ui/MainActivity.java
@@ -1,15 +1,75 @@
 package com.bry.power.ui;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.bry.power.R;
+import com.bry.power.service.MediaPlayerService;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayerService player;
+    boolean serviceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
     }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MediaPlayerService.LocalBinder binder = (MediaPlayerService.LocalBinder) service;
+            player = binder.getService();
+            serviceBound = true;
+
+            Toast.makeText(MainActivity.this,"Service Has Been Bound!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
+
+    private void playAudio(String media){
+        if(!serviceBound){
+            Intent playerIntent = new Intent(this,MediaPlayerService.class);
+            playerIntent.putExtra("media", media);
+            startService(playerIntent);
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        }else{
+
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putBoolean("serviceState",serviceBound);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        serviceBound = savedInstanceState.getBoolean("serviceState");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(serviceBound){
+            unbindService(serviceConnection);
+            player.stopSelf();
+        }
+    }
+
+
 }
