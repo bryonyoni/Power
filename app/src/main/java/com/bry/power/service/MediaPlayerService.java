ec4d1847d7d3d398ba2921d1c8896eb19e69e5d9
bryonyoni <bryonyoni@gmail.com>
2017-06-20 12:04:25 +0300

    Added the buildNotification method
    Added a build notificaion method for building actions and song information
    at the notification bar.

--- app/src/main/java/com/bry/power/service/MediaPlayerService.java
+++ app/src/main/java/com/bry/power/service/MediaPlayerService.java
@@ -1,5 +1,8 @@
 package com.bry.power.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -13,6 +16,7 @@ import android.media.session.MediaSessionManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
@@ -387,6 +391,65 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
         initMediaPlayer();
     }
 
+    private void buildNotification(PlaybackStatus playbackStatus){
+        int notificationAction = android.R.drawable.ic_media_pause;
+        PendingIntent play_pauseAction = null;
+
+        if(playbackStatus == PlaybackStatus.PLAYING){
+            notificationAction = android.R.drawable.ic_media_pause;
+            play_pauseAction = playbackAction(1);
+
+        }else if(playbackStatus == PlaybackStatus.PAUSED){
+            notificationAction = android.R.drawable.ic_media_play;
+            play_pauseAction = playbackAction(0);
+        }
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.owl);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setShowWhen(false)
+                .setStyle(new NotificationCompat.MediaStyle()
+                        .setMediaSession(mediaSession.getSessionToken())
+                        .setShowActionsInCompactView(0, 1, 2))
+                        .setColor(getResources().getColor(R.color.colorPrimary))
+                        .setlargeIcon(largeIcon)
+                        .setSmallIcon(android.R.drawable.stat_sys_headset)
+
+                        .setContentText(activeAudio.getArtist())
+                        .setContentTitle(activeAudio.getAlbum())
+                        .setContentInfo(activeAudio.getTitle())
+
+                        .addAction(android.R.drawable.ic_media_previous,"previous",playbackAction(3))
+                        .addAction(notificationAction,"pause",play_pauseAction)
+                        .addAction(android.R.drawable.ic_media_next,"next", playbackAction(2));
+
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private void removeNotification(){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private PendingIntent playbackAction(int actionNumber){
+        Intent playbackAction = new Intent(this,MediaPlayerService.class);
+        switch(actionNumber){
+            case 0:
+                playbackAction.setAction(ACTION_PLAY);
+                return PendingIntent.getService(this,actionNumber,playbackAction,0);
+            case 1:
+                playbackAction.setAction(ACTION_PAUSE);
+                return  PendingIntent.getService(this,actionNumber,playbackAction,0);
+            case 2:
+                playbackAction.setAction(ACTION_NEXT);
+                return PendingIntent.getService(this,actionNumber,playbackAction,0);
+            case 3:
+                playbackAction.setAction(ACTION_PREVIOUS);
+                return PendingIntent.getService(this,actionNumber,playbackAction,0);
+            default:
+                break;
+        }
+        return null;
+    }
 
  public class LocalBinder extends Binder {
         public MediaPlayerService getService(){
